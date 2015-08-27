7ceb17f767bdc34c99229edc9d7b90c8ce54893e
SephReed <ShadyWillowCreek@gmail.com>
2015-08-27 12:35:32 -0500

    funnies

--- thumbzart.go
+++ thumbzart.go
@@ -4,8 +4,14 @@ import "github.com/go-martini/martini"
 
 func main() {
   m := martini.Classic()
+
   m.Get("/", func() string {
     return "Hello world!"
   })
+
+  m.Get("/misty_bath", func() string {
+    return "THERE's SHIT EVERYWHERE!!!!!!"
+  })
+
   m.Run()
 }
