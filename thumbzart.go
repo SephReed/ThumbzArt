649ec49a5f22923beb0c5f1f611149e7819c3b26
SephReed <ShadyWillowCreek@gmail.com>
2015-08-28 11:48:08 -0500

    Started The Site

--- thumbzart.go
+++ thumbzart.go
@@ -1,17 +1,68 @@
 package main
 
-import "github.com/go-martini/martini"
+import (
+	"encoding/xml"
+    "net/http"
+    "html/template"
+
+    "github.com/labstack/echo"
+    "github.com/unrolled/render" // or "gopkg.in/unrolled/render.v1"
+)
+
+type ExampleXml struct {
+    XMLName xml.Name `xml:"example"`
+    One     string   `xml:"one,attr"`
+    Two     string   `xml:"two,attr"`
+}
+
+// var templates = template.Must(template.ParseGlob("templates/*"))
 
 func main() {
-  m := martini.Classic()
+	template.Must(template.ParseGlob("templates/sub_templates/*.tmpl"))
 
-  m.Get("/", func() string {
-    return "Hello world!"
-  })
+    r := render.New(render.Options{ IndentJSON: true, Layout: "layout" })
 
-  m.Get("/misty_bath", func() string {
-    return "THERE's SHIT EVERYWHERE!!!!!!"
-  })
+    e := echo.New()
 
-  m.Run()
+    // Routes
+    e.Get("/", func(c *echo.Context) error {
+        r.JSON(c.Response().Writer(), http.StatusOK, map[string]string{"welcome": "This is rendered JSON!"})
+        return nil
+    })
+
+    e.Get("/test", func(c *echo.Context) error {
+        c.Response().Writer().Write([]byte("Welcome, visit sub pages now."))
+        return nil
+    })
+
+    e.Get("/data", func(c *echo.Context) error {
+        r.Data(c.Response().Writer(), http.StatusOK, []byte("Some binary data here."))
+        return nil
+    })
+
+	e.Get("/text", func(c *echo.Context) error {
+		r.Text(c.Response().Writer(), http.StatusOK, "Plain text here")
+        return nil
+    })
+
+
+	e.Get("/html", func(c *echo.Context) error {
+		pageData := map[string]string{"name": "Seph", "css": "/css/test.css" };
+
+		r.HTML(c.Response().Writer(), http.StatusOK, "example", pageData)
+	    return nil
+    })
+
+
+	e.Static("/css/", "public/css")
+	e.Static("/images/", "public/images")
+	e.Favicon("images/favicon.ico")
+
+
+
+    e.Run(":3000")
 }
+
+
+
+
